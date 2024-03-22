9c386030d59f5301b00e10462d5f4e9af46f3574
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-03-22 17:27:09 +0200

    3-Layers Arch. Pattern - Business Logic Layer

---
+++ Route.C41.G01.BLL/Repcsitories/DepartmintRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G01.BLL.Interfaces;
+using Route.C41.G01.DAL.Data;
+using Route.C41.G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G01.BLL.Repcsitories
+{
+    internal class DepartmintRepository : IDepartmintRepository
+    {
+        private readonly ApplicationDpContext _dbcontext;
+
+        public DepartmintRepository(ApplicationDpContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+            
+        }
+
+        public int Add(Department entity)
+        {
+            _dbcontext.Add(entity);
+            return _dbcontext.SaveChanges();
+        }
+        public int Update(Department entity)
+        {
+            _dbcontext.Update(entity);
+            return _dbcontext.SaveChanges();
+        }
+
+        public int Delete(Department entity)
+        {
+            _dbcontext.Remove(entity);
+            return _dbcontext.SaveChanges();
+        }
+
+        public Department Get(int id)
+        {
+            return _dbcontext.Departments.Find(id);
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            return _dbcontext.Departments.AsNoTracking().ToList();
+        }
+
+       
+    }
+}
