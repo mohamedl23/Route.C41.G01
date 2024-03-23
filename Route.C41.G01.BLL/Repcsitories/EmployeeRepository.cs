c3ae2fcc7255aabbe950a24fb01345ee212cee56
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-03-24 00:37:32 +0200

    Part 06 Employee Repository - BLL

---
+++ Route.C41.G01.BLL/Repcsitories/EmployeeRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G01.BLL.Interfaces;
+using Route.C41.G01.DAL.Data;
+using Route.C41.G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G01.BLL.Repcsitories
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly ApplicationDpContext _dbcontext;
+
+        public EmployeeRepository(ApplicationDpContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+
+        }
+
+        public int Add(Employee entity)
+        {
+            _dbcontext.Add(entity);
+            return _dbcontext.SaveChanges();
+        }
+        public int Update(Employee entity)
+        {
+            _dbcontext.Update(entity);
+            return _dbcontext.SaveChanges();
+        }
+
+        public int Delete(Employee entity)
+        {
+            _dbcontext.Remove(entity);
+            return _dbcontext.SaveChanges();
+        }
+
+        public Employee Get(int id)
+        {
+            return _dbcontext.Employees.Find(id);
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbcontext.Employees.AsNoTracking().ToList();
+        }
+    }
+}
