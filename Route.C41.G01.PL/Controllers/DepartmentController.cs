4084ab8ab1d68f70a308d728ad3a6fd04ef4ab48
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-03-23 19:12:12 +0200

    Part 04 Department Controller - Delete

--- Route.C41.G01.PL/Controllers/DepartmentController.cs
+++ Route.C41.G01.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Route.C41.G01.BLL.Interfaces;
 using Route.C41.G01.DAL.Models;
 using System;
@@ -8,9 +10,12 @@ namespace Route.C41.G01.PL.Controllers
     public class DepartmentController : Controller
     {
         private readonly IDepartmintRepository _departmintRepository;
-        public DepartmentController(IDepartmintRepository departmintRepository)
+        private readonly IWebHostEnvironment _env;
+
+        public DepartmentController(IDepartmintRepository departmintRepository , IWebHostEnvironment env)
         {
             _departmintRepository = departmintRepository;
+           _env = env;
         }
 
         public IActionResult Index()
@@ -69,8 +74,8 @@ namespace Route.C41.G01.PL.Controllers
             return View(department);
             
         }
-        [ValidateAntiForgeryToken]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Edit([FromRoute]int id , Department department)
         {
             if ( id != department.Id)
@@ -97,6 +102,46 @@ namespace Route.C41.G01.PL.Controllers
             }
             return View(department);
         }
+        
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            var department = _departmintRepository.Get(id.Value);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return View(department);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(Department department)
+        {
+            try
+            {
+                _departmintRepository.Delete(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if (_env.IsDevelopment())
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Error");
+                }
+            }
+            return View(department);
+        }
+
+
 
     }
 }
