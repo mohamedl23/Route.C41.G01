7194d7642f2a08e3d6248683bf34eb7bc91c8948
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-03-27 00:13:00 +0200

    Employee Controller Views

---
+++ Route.C41.G01.DAL/Migrations/20240326220053_EmployeeAdded.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Route.C41.G01.DAL.Migrations
+{
+    public partial class EmployeeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
