d237ab1178ef7ffedf9e6396ec54100e2c2089c6
mohamedl23 <mhmed.mostafa550@gmail.com>
2024-04-30 01:58:21 +0300

    Account Controller - SignUp

--- Route.C41.G01.PL/Controllers/AccountController.cs
+++ Route.C41.G01.PL/Controllers/AccountController.cs
@@ -1,28 +1,62 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Route.C41.G01.DAL.Models;
 using Route.C41.G01.PL.ViewModels.User;
+using System.Threading.Tasks;
 
 namespace Route.C41.G01.PL.Controllers
 {
     public class AccountController : Controller
     {
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly SignInManager<ApplicationUser> _signInManager;
+
+		public AccountController(UserManager<ApplicationUser> userManager , SignInManager<ApplicationUser> signInManager)
+        {
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
         [HttpGet]
         public IActionResult SignUp()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult SignUp(SignUpViewModel model)
+        public async Task<IActionResult> SignUp(SignUpViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser()
+                var user = await _userManager.FindByNameAsync(model.Username);
+                if (user is null)
                 {
-                    UserName = model.Username,
-                    Email = model.Email,
+                    user = new ApplicationUser()
+                    {
+                        FName = model.FirstName,
+                        LName = model.LastName,
+                        UserName = model.Username,
+                        Email = model.Email,
+                        IsAgree = model.ISAgree
+                    };
 
-                };
-            }
+					var result = await _userManager.CreateAsync(user, model.Password);
+					if (result.Succeeded)
+					{
+						return RedirectToAction(nameof(SignIn));
+					}
+                    foreach(var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+				}
+				ModelState.AddModelError(string.Empty, "This User Is Already In Use For Another Account");
+
+				
+
+				    
+			}
+            
             return View();
         }
     }
